3858dbd2078dbb00fe33b768c48b431b9021dc09
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2023-09-07 23:10:38 +0530

    code edit auth

--- src/AuthApp/components/Home.js
+++ src/AuthApp/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
 
   return (
     <>
-      <h1>Home page</h1>
+      <h1>Home page Welcome here !!!</h1>
     </>
   );
 }
--- src/AuthApp/components/UserProfile.js
+++
@@ -1,5 +0,0 @@
-function UserProfile() {
-  return <div>User Profile</div>;
-}
-
-export default UserProfile;
